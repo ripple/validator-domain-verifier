476a10672626864fd6e9a7f86328bc681d13d4f2
wilsonianb <bwilson@ripple.com>
2017-07-10 15:39:13 -0700

    Update validators

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ function verifyDomain(domain, valPubKey, domainSig, valPubSig, certificate) {
   }
 
   try {
-    // execSync('openssl dgst -md5 -verify <(openssl x509 -in <(echo "' + certificate + '") -pubkey -noout) -signature <(xxd -r -p <(echo "'+valPubSig+'")) <(echo '+valPubKey+') 2>/dev/null', { "shell": "/bin/bash" })
     execSync('openssl dgst -verify <(openssl x509 -in <(echo "' + certificate + '") -pubkey -noout) -signature <(xxd -r -p <(echo "'+valPubSig+'")) <(echo '+valPubKey+') 2>/dev/null', { "shell": "/bin/bash" })
+    // console.log(execSync('openssl dgst -verify <(openssl x509 -in <(echo "' + certificate + '") -pubkey -noout) -signature <(xxd -r -p <(echo "'+valPubSig+'")) <(echo '+valPubKey+') 2>/dev/null', { "shell": "/bin/bash" }))
   } catch (err) {
     throw 'invalid SSL signature'
   }
